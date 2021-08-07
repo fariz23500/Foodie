c58dfc37321fc8f69d812bfb47ae25c4d7f2fb98
Fariz Ali <farizali23500@gmail.com>
2021-08-08 01:49:11 +0530

    sixth commit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, './my-app/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
+    res.sendFile(path.join(__dirname, './my-app/build/public/index.html'));
   });
 
 let d=[];
